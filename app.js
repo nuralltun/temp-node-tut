b8467cc253eb02a40f0604c041dd9a74d2a9188d
NurAltun <nuralltun>
2022-10-29 14:36:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//underscore
+
+// var _=require('underscore');
+// var numbers = [10, 5, 100, 2, 1000];
+// console.log(_.min(numbers));
+
+
+// var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+// console.log(_.sortBy(stooges, 'name'));
